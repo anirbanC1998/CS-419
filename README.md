73e062fed3f8ae7b4e8cc4129836bf432c7339fb
Kenny Cho <kennycho@Kennys-MBP.fios-router.home>
2020-04-19 15:05:44 -0400

    Kenny's updated python3 script. Now with md5 hash

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CS419 System Security
 CS 419 System Security Project
+Please Run Using Python3
--- script.py
+++ script.py
@@ -1,30 +1,67 @@
 import os
 import sys
+import subprocess
 
 def theUserEnteredOneOfTheOptions(the_option):
-   if the_option == "protect":
-      return True
-   if the_option == "unprotect":
-      return True
-   return False
+    if the_option == "protect":
+       return True
+    if the_option == "unprotect":
+       return True
+    return False
 
-"""encrypts and sets permissions to owner ownly; removes the original file; results in a file like (test.txt.gpg)"""
+# encrypts and sets permissions to owner ownly; removes the original file; results in a file like (test.txt.gpg)
 def protectFile(file):
-   os.system("gpg -c" + " " + file)
-   os.system("rm" + " " + file)
-   os.system("chmod 700" + " " + file + ".gpg")
+    md5sum_result = subprocess.run(["md5sum", file], stderr=subprocess.DEVNULL)
+    if md5sum_result.returncode != 0:
+       print("md5 hash for file failed!")
+       sys.exit()
+    else:
+       print("Please copy the preceding md5 hash value for your file to check file integrity. It should be the same after unprotect is executed.")
 
-"""decrypts and sets permissions to owner only; removes a file like (test.txt.gpg); results in a file like (test.txt)"""
+    gpg_result = subprocess.run(["gpg", "-c", file], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if gpg_result.returncode != 0:
+       print("gpg passphrase encryption failed!")
+       sys.exit()
+
+    rm_result = subprocess.run(["rm", file], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if rm_result.returncode != 0:
+       print("removing the original file failed!")
+       sys.exit()
+
+    chmod_result = subprocess.run(["chmod", "700", file + ".gpg"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if chmod_result.returncode != 0:
+       print("setting permissions to owner only on encrypted file failed!")
+       sys.exit()
+
+# decrypts and sets permissions to owner only; removes a file like (test.txt.gpg); results in a file like (test.txt)
 def unprotectFile(file):
-   os.system("gpg" + " " + file + ".gpg")
-   os.system("rm" + " " + file + ".gpg")
-   os.system("chmod 700" + " " + file)
+    gpg_result = subprocess.run(["gpg", file + ".gpg"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if gpg_result.returncode != 0:
+       print("gpg passphrase decryption failed!")
+       sys.exit()
+
+    md5sum_result = subprocess.run(["md5sum", file], stderr=subprocess.DEVNULL)
+    if md5sum_result.returncode != 0:
+       print("md5 hash for file failed!")
+       sys.exit()
+    else:
+       print("Please compare your previously generated md5 hash with the preceding md5 hash. If they are NOT the same, then file contents have likely been compromised.")
+
+    rm_result = subprocess.run(["rm", file + ".gpg"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if rm_result.returncode != 0:
+       print("removing the encrypted file failed!")
+       sys.exit()
+
+    chmod_result = subprocess.run(["chmod", "700", file], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if chmod_result.returncode != 0:
+       print("setting permissions to owner only on decrypted file failed!")
+       sys.exit()
 
 def executeOption(ex_option, ex_file):
-   if ex_option == "protect":
-      protectFile(ex_file)
-   if ex_option == "unprotect":
-      unprotectFile(ex_file)
+    if ex_option == "protect":
+       protectFile(ex_file)
+    if ex_option == "unprotect":
+       unprotectFile(ex_file)
 
 print("Type one of the following options: protect, unprotect")
 
@@ -34,7 +71,10 @@ if theUserEnteredOneOfTheOptions(option) == False:
    print("You did not enter one of the options!")
    sys.exit()
 
-print("What is the name of the file you want to:" + " " + option)
+if option == "protect":
+   print("What is the name of the file you want to protect?")
+if option == "unprotect":
+   print("What is the name of the file you want to unprotect? Please enter the original name of the file.")
 
 file = input()
 
